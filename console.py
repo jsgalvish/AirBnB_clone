e58af77d8c695ad66d1c28022bf7fef126313126
jsgalvish <jsgalvish@unal.edu.co>
2019-11-12 02:47:59 -0500

    6. Console 0.0.1
    Co-authored-by: Miguel Salazar <miguelangel697@gmail.com>
    Co-authored-by: Sebastian Galvis <jsgalvish@unal.edu.co>"

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF to exit the program"""
+        return True
+
+    def emptyline(self):
+        """ emptyline do nothing """
+        return False
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

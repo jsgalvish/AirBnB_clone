e8906edcca6a5e85f1d431b14c12e17619c20f4c
jsgalvish <jsgalvish@unal.edu.co>
2019-11-12 01:53:40 -0500

    4. Create BaseModel from dictionary
    Co-authored-by: Miguel Salazar <miguelangel697@gmail.com>
    Co-authored-by: Sebastian Galvis <jsgalvish@unal.edu.co>"

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,19 @@ time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)

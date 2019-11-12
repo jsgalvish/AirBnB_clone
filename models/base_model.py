de36bf4524879065c490b13f17b38d8d9fbdf126
jsgalvish <jsgalvish@unal.edu.co>
2019-11-12 02:28:15 -0500

    5. Store first object
    Co-authored-by: Miguel Salazar <miguelangel697@gmail.com>
    Co-authored-by: Sebastian Galvis <jsgalvish@unal.edu.co>"

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 import uuid
+import models
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -21,6 +22,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import json
+
+
+class FileStorage:
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
+
+    def save(self):
+        j__objects = {}
+        for key in self.__objects:
+            j__objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as __file:
+            json.dump(j__objects, __file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as __file:
+                j__objects = json.load(__file)
+            for key in j__objects:
+                self.__objects[key] = classes[j__objects[key]
+                                              ["__class__"]](**j__objects[key])
+        except:
+            pass

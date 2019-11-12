8fd51b379ad9b9811faa79187975d0a350a80168
jsgalvish <jsgalvish@unal.edu.co>
2019-11-12 16:19:45 -0500

    10. Console 1.0
    Co-authored-by: Miguel Salazar <miguelangel697@gmail.com>
    Co-authored-by: Sebastian Galvis <jsgalvish@unal.edu.co>"

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 import json
-from models.user import User
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity, "City": City, "Place": Place, "Review": Review, "State": State}
 
 
 class FileStorage:
